ff7becff3610649dc856cea0a461599a2c432c9d
Evans-Gash <evansgash88@gmail.com>
2023-04-11 14:01:11 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,22 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+	int num1, num2, result;
 
-    /* Check if the program was passed two arguments */
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
+	/* Check if the program was passed two arguments */
+		if (argc != 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-    /* Convert the arguments to integers and multiply them */
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    result = num1 * num2;
+	/* Convert the arguments to integers and multiply them */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
-    /* Print the result followed by a newline */
-    printf("%d\n", result);
+	/* Print the result followed by a newline */
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
