cc201fca6aead2a723e895b3a9a10a5b6485b90e
Evans-Gash <evansgash88@gmail.com>
2023-04-11 14:19:55 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,46 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
- * main - Adds positive numbers passed as command-line arguments
- * @argc: Number of arguments passed to the program
- * @argv: Array of arguments passed to the program
+ * main - a program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argumrnt vector
  *
- * Return: 0 on success, 1 on error
+ * - Print the result, followed by a new line
+ * - If no number is passed to the program, print 0, followed by a new line
+ * - If one of the number contains symbols that are not digits, print Error,
+ *   followed by a new line, and return 1
+ *
+ * Return: 0 (success)
  */
+
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i, j;
+	int c, j, add = 0;
 
-    /* Check if any arguments were passed */
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (c = 1; c < argc; c++)
+		{
+			j = 0;
 
-    /* Iterate through all arguments and add positive numbers */
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+			while (argv[c][j])
+			{
+				if (!isdigit(argv[c][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				j++;
+			}
 
-        if (atoi(argv[i]) > 0)
-        {
-            sum += atoi(argv[i]);
-        }
-    }
+			add = add + atoi(argv[c]);
+		}
 
-    /* Print the result followed by a newline */
-    printf("%d\n", sum);
+		if (add > 0)
+			printf("%d\n", add);
+	}
 
-    return (0);
+	return (0);
 }
