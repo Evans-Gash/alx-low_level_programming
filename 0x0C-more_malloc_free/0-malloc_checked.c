fdf2f6560c64b507f47251abf6053f95977eadc4
Evans-Gash <evansgash88@gmail.com>
2023-04-13 12:39:28 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>   /* for malloc */
+#include <stdio.h>    /* for fprintf and exit */
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
