8284a10ba2578642180d2a09de04ceab80fa2197
Evans-Gash <evansgash88@gmail.com>
2023-04-17 11:29:37 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+
 /**
- * new_dog - creates a new dog
- * @name: pointer to dog's name
- * @age: dog's age
- * @owner: pointer to dog's owner name
+ * new_dog - makes a dog
  *
- * Return: pointer to the new dog, or NULL if function fails
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *new_name, *new_owner;
-	int name_len, owner_len;
+	dog_t *d;
+	int len;
+	char *ptr;
 
-	/* Allocate memory for the new dog */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (name == 0 || owner == 0)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
 
-	/* Find the lengths of the name and owner strings */
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
-		;
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
-		;
-
-	/* Allocate memory for the copied name and owner strings */
-	new_name = malloc(sizeof(char) * (name_len + 1));
-	if (new_name == NULL)
+	for (len = 1, ptr = name; *ptr; len++)
+		ptr++;
+	d->name = malloc(len);
+	if (d->name == 0)
 	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(new_dog);
+		free(d);
 		return (NULL);
 	}
 
-	/* Copy the name and owner strings */
-	for (int i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
-	for (int i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
+	for (len = 1, ptr = owner; *ptr; len++)
+		ptr++;
+	d->owner = malloc(len);
+	if (d->owner == 0)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 
-	/* Set the dog's properties */
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
+	for (len = 0; *name != 0; len++, name++)
+		d->name[len] = *name;
+	d->name[len] = 0;
+	for (len = 0; *owner != 0; len++)
+		d->owner[len] = *owner++;
+	d->owner[len] = 0;
+	d->age = age;
 
-	return (new_dog);
+	return (d);
 }
