41faa3be66befd92355ae536c23f0f724874ca7f
Evans-Gash <evansgash88@gmail.com>
2023-04-19 09:25:58 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array == NULL || action == NULL)
-	return;
+		return;
 
 	for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+	{
+		action(array[i]);
+	}
 }
