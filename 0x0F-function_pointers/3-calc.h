e691bfe5bf4601a6f4f30b518d54c00ed4a85445
Evans-Gash <evansgash88@gmail.com>
2023-04-19 13:26:46 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,29 +1,23 @@
 #ifndef CALC_H
-#define CALC_H
-
-#include <stdio.h>
-#include <stdlib.h>
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-/**
- * struct op - Struct op
-@@ -16,13 +20,4 @@ typedef struct op
-	int (*f)(int a, int b);
-} op_t;
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-int (*get_op_func(char *s))(int, int);
+	#define CALC_H
+	/**
+	 * struct op - A struct op.
+	 * @op: The operator.
+	 * @f: The associated function.
+	 */
+	typedef struct op
+	{
+		char *op;
+		int (*f)(int a, int b);
+	} op_t;
 
 
-#endif /* CAL_H */
-#endif
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+	int (*get_op_func(char *s))(int, int);
+
+
+	#endif
